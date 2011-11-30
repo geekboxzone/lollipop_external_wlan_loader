4668920ff7fbaf71bb3399fc58edf7fda13b60d3
hwg <hwg@rock-chips.com>
2011-11-30 09:36:11 +0800

    add atheros ar6003 firmware

--- wifi-firmware.mk
+++ wifi-firmware.mk
@@ -10,6 +10,12 @@ PRODUCT_COPY_FILES += \
         external/wlan_loader/firmware/sd8686.bin:system/etc/firmware/sd8686.bin \
         external/wlan_loader/firmware/sd8686_helper.bin:system/etc/firmware/sd8686_helper.bin \
         external/wlan_loader/firmware/fw_bcm4319.bin:system/etc/firmware/fw_bcm4319.bin \
-        external/wlan_loader/firmware/nvram.txt:system/etc/firmware/nvram.txt
+        external/wlan_loader/firmware/nvram.txt:system/etc/firmware/nvram.txt \
+        external/wlan_loader/firmware/athtcmd_ram.bin:system/etc/firmware/athtcmd_ram.bin \
+        external/wlan_loader/firmware/athwlan.bin.z77:system/etc/firmware/athwlan.bin.z77 \
+        external/wlan_loader/firmware/bdata.SD31.bin:system/etc/firmware/bdata.SD31.bin \
+        external/wlan_loader/firmware/data.patch.hw2_0.bin:system/etc/firmware/data.patch.hw2_0.bin \
+        external/wlan_loader/firmware/device.bin:system/etc/firmware/device.bin \
+        external/wlan_loader/firmware/otp.bin.z77:system/etc/firmware/otp.bin.z77
 
 
