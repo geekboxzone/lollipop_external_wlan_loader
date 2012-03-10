3217cb37ba32f76235bd1c69802a8a5b5956a656
wdc <wdc@rock-chips.com>
2012-03-10 16:43:33 +0800

    wifi: add bcm4329_wapi fw

--- wifi-firmware.mk
+++ wifi-firmware.mk
@@ -4,7 +4,6 @@ PRODUCT_COPY_FILES += \
         external/wlan_loader/wlan.ko:system/lib/modules/wlan.ko \
         external/wlan_loader/iwconfig:system/bin/iwconfig \
         external/wlan_loader/iwlist:system/bin/iwlist \
-        external/wlan_loader/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
         external/wlan_loader/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin \
         external/wlan_loader/firmware/nvram_B23.txt:system/etc/firmware/nvram_B23.txt \
         external/wlan_loader/firmware/sd8686.bin:system/etc/firmware/sd8686.bin \
@@ -16,6 +15,14 @@ PRODUCT_COPY_FILES += \
         external/wlan_loader/firmware/bdata.SD31.bin:system/etc/firmware/bdata.SD31.bin \
         external/wlan_loader/firmware/data.patch.hw2_0.bin:system/etc/firmware/data.patch.hw2_0.bin \
         external/wlan_loader/firmware/device.bin:system/etc/firmware/device.bin \
-        external/wlan_loader/firmware/otp.bin.z77:system/etc/firmware/otp.bin.z77
+        external/wlan_loader/firmware/otp.bin.z77:system/etc/firmware/otp.bin.z77 \
+        external/wlan_loader/firmware/fw_bcmdhd.bin:/system/etc/firmware/fw_bcmdhd.bin \
+        external/wlan_loader/firmware/bcmdhd.cal:/system/etc/firmware/bcmdhd.cal
+
+ifeq ($(WIFI_HAVE_WAPI), true)
+       PRODUCT_COPY_FILES +=  external/wlan_loader/firmware/fw_bcm4329_wapi.bin:system/etc/firmware/fw_bcm4329.bin 
+else
+        PRODUCT_COPY_FILES += external/wlan_loader/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin
+endif
 
 
