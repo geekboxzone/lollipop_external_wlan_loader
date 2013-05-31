81dc6e276dda7480a5d8287ab109bc846eb0a678
hwg <hwg@rock-chips.com>
2013-05-31 16:07:03 +0800

    delete fw_bcm4329.bin copy

--- wifi-firmware.mk
+++ wifi-firmware.mk
@@ -8,10 +8,10 @@ WifiFirmwareFile := $(shell ls $(LOCAL_PATH)/firmware)
 PRODUCT_COPY_FILES += \
         $(foreach file, $(WifiFirmwareFile), $(LOCAL_PATH)/firmware/$(file):system/etc/firmware/$(file))
 
-ifeq ($(WIFI_HAVE_WAPI), true)
-       PRODUCT_COPY_FILES +=  external/wlan_loader/firmware/fw_bcm4329_wapi.bin:system/etc/firmware/fw_bcm4329.bin 
-else
-        PRODUCT_COPY_FILES += external/wlan_loader/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin
-endif
+#ifeq ($(WIFI_HAVE_WAPI), true)
+#       PRODUCT_COPY_FILES +=  external/wlan_loader/firmware/fw_bcm4329_wapi.bin:system/etc/firmware/fw_bcm4329.bin 
+#else
+#        PRODUCT_COPY_FILES += external/wlan_loader/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin
+#endif
 
 
